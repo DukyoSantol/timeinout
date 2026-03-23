6b038f1ca60384a1002d5035a1222e0cb9b87b18
DukyoSantol <dukyosantol@gmail.com>
2026-03-23 11:19:21 +0800

    3-23-2026 (1)

--- routes/web.php
+++ routes/web.php
@@ -24,27 +24,6 @@ Route::get('/csrf-token', function() {
 // Time record routes (protected)
 Route::middleware('auth')->group(function () {
     Route::get('/time-in-out', [TimeRecordController::class, 'create'])->name('time-records.form');
-    Route::get('/time-in-out-simple', function() {
-        return view('time-records.form_simple');
-    })->name('time-records.form-simple');
-    Route::get('/test-minimal', function() {
-        return view('time-records.test_minimal');
-    })->name('time-records.test-minimal');
-    Route::get('/test-isolated', function() {
-        return view('time-records.test_isolated');
-    })->name('time-records.test-isolated');
-    Route::get('/test-form', function() {
-        return view('test_form');
-    })->name('test.form');
-    Route::get('/test-simple', function() {
-        return view('test_simple');
-    })->name('test.simple');
-    Route::get('/time-test', function() {
-        return view('time_test');
-    })->name('time.test');
-    Route::get('/simple-time-test', function() {
-        return view('simple_time_test');
-    })->name('simple.time.test');
     Route::post('/time-records', [TimeRecordController::class, 'store'])->name('time-records.store');
     Route::post('/time-records/time-out-user', [TimeRecordController::class, 'timeOutUser'])->name('time-records.time-out-user');
     Route::post('/check-timed-in-status', [TimeRecordController::class, 'checkTimedInStatus'])->name('time-records.check-timed-in-status');
@@ -64,24 +43,13 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [TimeRecordController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/export', [TimeRecordController::class, 'exportTimeRecords'])->name('admin.export');
-    Route::get('/test-export', [TimeRecordController::class, 'exportTimeRecords'])->name('admin.test.export');
     Route::get('/simple-export', [TimeRecordController::class, 'simpleExport'])->name('admin.simple.export');
     Route::get('/view-csv', [TimeRecordController::class, 'viewCsv'])->name('admin.view.csv');
     Route::post('/store-export-filters', [TimeRecordController::class, 'storeExportFilters'])->name('admin.store.export.filters');
     
-    // Debug test route
-    Route::get('/debug-params', function(Request $request) {
-        \Log::info('=== DEBUG ROUTE ===');
-        \Log::info('URL: ' . $request->fullUrl());
-        \Log::info('Query: ' . $request->getQueryString());
-        \Log::info('All params: ' . json_encode($request->all()));
-        return response()->json([
-            'url' => $request->fullUrl(),
-            'query' => $request->getQueryString(),
-            'params' => $request->all()
-        ]);
-    })->name('admin.debug.params');
     Route::delete('/time-records/{id}', [TimeRecordController::class, 'deleteRecord'])->name('admin.time-records.delete');
+    
+    // User Management
     Route::get('/users', [TimeRecordController::class, 'indexUsers'])->name('admin.users.index');
     Route::get('/users/create', [TimeRecordController::class, 'createUser'])->name('admin.users.create');
     Route::post('/users', [TimeRecordController::class, 'storeUser'])->name('admin.users.store');
@@ -89,14 +57,17 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::put('/users/{id}', [TimeRecordController::class, 'updateUser'])->name('admin.users.update');
     Route::delete('/users/{id}', [TimeRecordController::class, 'deleteUser'])->name('admin.users.delete');
     Route::get('/users/export', [TimeRecordController::class, 'exportUsers'])->name('admin.users.export');
+    Route::get('/users/{id}/change-password', [TimeRecordController::class, 'showChangePasswordForm'])->name('admin.users.change-password');
+    Route::post('/users/{id}/change-password', [TimeRecordController::class, 'changePassword'])->name('admin.users.change-password.post');
+    
+    // User Records (Sidebar View)
+    Route::get('/users/records-sidebar', [TimeRecordController::class, 'userRecordsSidebar'])->name('admin.users.sidebar');
+    
+    // Time Records Management
     Route::get('/time-records', [TimeRecordController::class, 'timeRecordsIndex'])->name('admin.time-records.index');
     Route::get('/time-records/{id}/edit', [TimeRecordController::class, 'edit'])->name('admin.time-records.edit');
     Route::put('/time-records/{id}', [TimeRecordController::class, 'update'])->name('admin.time-records.update');
     Route::delete('/time-records/{id}', [TimeRecordController::class, 'destroy'])->name('admin.time-records.destroy');
     Route::post('/time-records/{id}/time-out', [TimeRecordController::class, 'timeOut'])->name('admin.time-records.time-out');
     Route::get('/filter', [TimeRecordController::class, 'filter'])->name('admin.time-records.filter');
-    Route::get('/users/{id}/change-password', [TimeRecordController::class, 'showChangePasswordForm'])->name('admin.users.change-password');
-    Route::post('/users/{id}/change-password', [TimeRecordController::class, 'changePassword'])->name('admin.users.change-password.post');
-    Route::get('/users/{id}/records', [TimeRecordController::class, 'userRecords'])->name('admin.users.records');
-    Route::get('/users/records-sidebar', [TimeRecordController::class, 'userRecordsSidebar'])->name('admin.users.sidebar');
 });
